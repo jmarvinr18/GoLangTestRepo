282872832913d3cf24c672d6751b2ab351462261
jmarvinr <jmarvin.ramoda@gmail.com>
2020-02-17 16:30:40 +0800

    go hello world

---
+++ src/github.com/jmarvinr18/hello-world/Main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello everyone")
+
+	foo()
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+	bar() 
+}
+
+
+func foo()  {
+	fmt.Println("I'm in foo")
+}
+
+func bar()  {
+	fmt.Println("and then we exited")
+	
+}
+// control flow
